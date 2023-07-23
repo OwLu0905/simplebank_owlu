8c1a883c72d2d07c55ff2b28c15b75846b0f1410
OwLu0905 <owlu0905@gmail.com>
2023-07-23 21:06:06 +0800

    add sql query & go mod

---
+++ db/query/transfer.sql
@@ -0,0 +1,24 @@
+-- name: CreateTransfer :one 
+INSERT INTO transfers (
+ from_account_id,
+ to_account_id,
+ amount
+) VALUES (
+  $1, $2, $2
+) RETURNING *;
+
+-- name: GetTransfer :one
+select *
+from transfers
+where id = $1
+limit 1
+;
+
+-- name: ListTransfers :many
+select *
+from transfers
+where from_account_id = $1 or to_account_id = $2
+order by id
+limit $3
+offset $4
+;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OwLu0905/simplebank_owlu
+
+go 1.20
