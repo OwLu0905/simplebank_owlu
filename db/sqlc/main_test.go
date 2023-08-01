9438f1a167db3750f2cab6ba541aa15a455213b9
OwLu0905 <owlu0905@gmail.com>
2023-08-01 22:31:18 +0800

    add transaction without checking balance

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -16,13 +16,17 @@ const (
 
 var testQueries *Queries
 
+// NOTE : In order to reuse the db connection, we should declare a new global variable (then we could export this variable)
+var testDB *sql.DB
+
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 
